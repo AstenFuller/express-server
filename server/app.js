b63f45faec69f5d66e54fca9d3bdf2035aa6f6d5
Asten Fuller <astenhfuller@gmail.com>
2020-02-13 10:41:46 -0800

    working

--- server/app.js
+++ server/app.js
@@ -1,9 +1,24 @@
-// import files and packages up here
+const express = require('express');
+const morgan = require('morgan');
 
+var bodyParser = require('body-parser');
 
-// create your express server below
-var app;
+const app = express();
+const json = require('./data.json')
 
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.status(200).type('.html').end();
+});
+
+app.get('/data', (req, res) => {
+    res.send(json);
+});
 // add your routes and middleware below
 
 // finally export the express application
